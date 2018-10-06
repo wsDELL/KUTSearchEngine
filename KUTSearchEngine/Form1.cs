d8ea1aa251732fbf04e9a5430706cf1902fcf9a5
“wsDELL” <sunyuxiang524@gmail.com>
2018-10-06 11:43:37 +1000

    Add project files.

---
+++ KUTSearchEngine/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace KUTSearchEngine
+{
+    public partial class Form1 : Form
+    {
+        private List<string[]> fileContent = new List<string[]>();
+        private string indexPath = "";
+        private LuceneAdvancedSearchApplication myLuceneApp = new LuceneAdvancedSearchApplication();
+    
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+         
+            
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BroweButton1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            string path = folderBrowserDialog1.SelectedPath;
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            FileInfo[] file = directoryInfo.GetFiles();
+
+            foreach (FileInfo info in file)
+            {
+
+                
+                if (info.Extension.ToLower() == ".txt")
+                {
+                    string content = File.ReadAllText(info.FullName);
+                    string[] contents = content.Split('T', 'I', 'A', 'B', 'W');
+                    string[] newList = new string[6];
+                    Array.Copy(contents, 1, newList, 0, 5);
+                    for (int i = 1; i < newList.Length-1; i++)
+                    {
+                        newList[i] = newList[i].Substring(1, newList[i].Length - 1);
+                        newList[i] = newList[i].Replace("\n", "").Replace("\r", " ");
+                        
+
+                    }
+                    newList[newList.Length-2] = newList[newList.Length-2].Replace(newList[1],"");
+                    string[] splitAbstract = newList[newList.Length - 2].Split('.');
+                    newList[newList.Length - 1] = splitAbstract[0] + ".";
+                    fileContent.Add(newList);
+                }
+            }
+
+            
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IndexBroweButton1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog2.ShowDialog();
+            indexPath = folderBrowserDialog2.SelectedPath;
+           
+            myLuceneApp.CreateIndex(indexPath);
+            int docID = 0;
+            string reminder = "";
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            foreach (string[] s in fileContent)
+            {
+
+                reminder+="Adding doc " + docID + ". to Index\n";
+                myLuceneApp.IndexText(s);
+                docID++;
+            }
+            stopwatch.Stop();
+
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            double time=timeSpan.Milliseconds;
+            label2.Text = time+"ms";
+            label3.Text="All documents added.";
+            myLuceneApp.CleanUpIndexer();
+        }
+
+
+
+        private void InfoNeedInput_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            string infoNeed = InfoNeedInput.Text;
+            myLuceneApp.CreateSearcher();
+            Lucene.Net.Search.Query query = myLuceneApp.InfoParser(infoNeed);
+            queryDisplay.Text = query.ToString();
+            Lucene.Net.Search.TopDocs result= myLuceneApp.SearchText(query);
+            
+            resultDisplaylistBox.Text += result.MaxScore;
+            resultDisplaylistBox.Text += "Number of results is " + result.TotalHits;
+            int rank = 0;
+            foreach (Lucene.Net.Search.ScoreDoc scoreDoc in result.ScoreDocs)
+            {
+                rank++;
+                Lucene.Net.Documents.Document doc =myLuceneApp.GetSearcher.Doc(scoreDoc.Doc);
+                string title = doc.Get("title").ToString();
+                string author = doc.Get("author").ToString();
+                string bbibliographic = doc.Get("bibliographic").ToString();
+                string textAbstract = doc.Get("firstSentence").ToString();
+                resultDisplaylistBox.Text+=("title: " + title +"\n"+  " author: " + author+"\n"+ " bbibliographic: " + bbibliographic+"\n"+ textAbstract+"\n\n");
+                //Lucene.Net.Search. Explanation explanation = myLuceneApp.Searcher.Explain(query, scoreDoc.Doc);
+                //resultDisplay.Text+= explanation.ToString();
+                //" text: " + myFieldValue +
+            }
+            myLuceneApp.CleanUpSearcher();
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ KUTSearchEngine/LuceneApplication.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis; // for Analyser
+using Lucene.Net.Documents; // for Document and Field
+using Lucene.Net.Index; //for Index Writer
+using Lucene.Net.Store; //for Directory
+using Lucene.Net.Search; // for IndexSearcher
+using Lucene.Net.QueryParsers;  // for QueryParser
+using Lucene.Net.Analysis.Snowball; // for snowball analyser 
+
+namespace KUTSearchEngine
+{
+    class LuceneAdvancedSearchApplication
+    {
+        Lucene.Net.Store.Directory luceneIndexDirectory;
+        Lucene.Net.Analysis.Analyzer analyzer;
+        Lucene.Net.Index.IndexWriter writer;
+        IndexSearcher searcher;
+        QueryParser parser;
+
+        const Lucene.Net.Util.Version VERSION = Lucene.Net.Util.Version.LUCENE_30;
+        const string TEXT_FN = "abstract";
+
+
+        public LuceneAdvancedSearchApplication()
+        {
+            luceneIndexDirectory = null;
+            writer = null;
+            //analyzer = new Lucene.Net.Analysis.SimpleAnalyzer();
+            analyzer = new Lucene.Net.Analysis.Standard.StandardAnalyzer(VERSION);
+            //analyzer = new Lucene.Net.Analysis.Snowball.SnowballAnalyzer(VERSION, "English");
+            parser = new QueryParser(Lucene.Net.Util.Version.LUCENE_30, TEXT_FN, analyzer);
+
+        }
+        /// <summary>
+        /// Creates the index at a given path
+        /// </summary>
+        /// <param name="indexPath">The pathname to create the index</param>
+        public void CreateIndex(string indexPath)
+        {
+
+            luceneIndexDirectory = Lucene.Net.Store.FSDirectory.Open(indexPath);
+            IndexWriter.MaxFieldLength mfl = new IndexWriter.MaxFieldLength(IndexWriter.DEFAULT_MAX_FIELD_LENGTH);
+            writer = new Lucene.Net.Index.IndexWriter(luceneIndexDirectory, analyzer, true, mfl);
+        }
+
+        /// <summary>
+        /// Indexes a given string into the index
+        /// </summary>
+        /// <param name="text">The text to index</param>
+        public void IndexText(string[] text)
+        {
+            Lucene.Net.Documents.Document doc = new Document();
+
+
+
+            doc.Add(new Field("id", text[0], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES));
+            doc.Add(new Field("title", text[1], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES));
+            doc.Add(new Field("author", text[2], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES));
+            doc.Add(new Field("bibliographic", text[3], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES));
+            doc.Add(new Field("abstract", text[4], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES));
+            doc.Add(new Field("firstSentence", text[5], Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES)); 
+            writer.AddDocument(doc);
+        }
+
+        /// <summary>
+        /// Flushes the buffer and closes the index
+        /// </summary>
+        public void CleanUpIndexer()
+        {
+            writer.Optimize();
+            writer.Flush(true, true, true);
+            writer.Dispose();
+        }
+
+
+        /// <summary>
+        /// Creates the searcher object
+        /// </summary>
+        public void CreateSearcher()
+        {
+            searcher = new IndexSearcher(luceneIndexDirectory);
+        }
+
+        /// <summary>
+        /// Searches the index for the querytext
+        /// </summary>
+        /// <param name="querytext">The text to search the index</param>
+        public Query InfoParser(string infoNeed)
+        {
+            System.Console.WriteLine("Searching for " + infoNeed);
+            infoNeed = infoNeed.ToLower();
+            Query query = parser.Parse(infoNeed);
+            return query;
+        }
+
+        public TopDocs SearchText(Query query)
+        {
+
+
+            TopDocs results = searcher.Search(query, 1400);
+
+            return results;
+
+
+        }
+
+        public void DisplayOneResult(TopDocs results, Query query)
+        {
+
+            System.Console.WriteLine("Number of results is " + results.TotalHits);
+            int rank = 0;
+            foreach (ScoreDoc scoreDoc in results.ScoreDocs)
+            {
+                rank++;
+                Lucene.Net.Documents.Document doc = searcher.Doc(scoreDoc.Doc);
+                string myFieldValue = doc.Get(TEXT_FN).ToString();
+                Console.WriteLine("Rank: " + rank + " text: " + myFieldValue + " Score: " + scoreDoc.Score + " Doc-id: " + scoreDoc.Doc);
+                Explanation explanation = searcher.Explain(query, scoreDoc.Doc);
+                Console.WriteLine(explanation.ToString());
+
+            }
+
+        }
+
+        public Searcher GetSearcher 
+        {
+            get
+            {
+                return searcher;
+            }
+        }
+
+        /// <summary>
+        /// Closes the index after searching
+        /// </summary>
+        public void CleanUpSearcher()
+        {
+            searcher.Dispose();
+        }
+        /*
+        public Directory LuceneIndexDirectory
+        {
+            get
+            {
+                return luceneIndexDirectory;
+            }
+            set
+            {
+                luceneIndexDirectory = value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Hello Lucene.Net");
+
+            LuceneApplication myLuceneApp = new LuceneApplication();
+            string indexPath = @"C:\Lucene\MyIndex";
+            LuceneApplication luceneApplication = new LuceneApplication();
+            luceneApplication.OpenIndex(indexPath);
+
+            luceneApplication.CreateAnalyser();
+            luceneApplication.CreateWriter();
+
+            string text = "The Daily Star";
+            luceneApplication.IndexText(text);
+            luceneApplication.CleanUp();
+
+
+
+
+
+            System.Console.ReadLine();
+        }
+        */
+    }
+}
